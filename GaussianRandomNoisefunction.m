493ded74b9e157777fa08697908d70c48e76462f
HuiHu1 <43657104+HuiHu1@users.noreply.github.com>
2020-01-18 21:41:21 -0700

    Update GaussianRandomNoisefunction.m

--- GaussianRandomNoisefunction.m
+++ GaussianRandomNoisefunction.m
@@ -1,7 +1,7 @@
 %--------------------Adding gaussian noise into covariance value---------
 function [new_fair,new_unfair] = GaussianRandomNoisefunction(total,train_x,train_Sensitive,R,featureNum,covariance)
 u1=0; 
-sigma1=1; % control the noise
+sigma1=0.1; % control the noise
 R1 = normrnd(u1,sigma1,total,1);
 for i=1:total
     Y_hat(:,i)=train_x*R(:,i);
@@ -14,8 +14,7 @@ new_unfair = zeros(featureNum,total);
 for i=1:total
     if(covariance_after(i,1)>0 && covariance_after(i,1)<=covariance)
         new_fair(:,i) = R(:,i); 
-    end
-    if(covariance_after(i,1)>covariance)
+    else
         new_unfair(:,i) = R(:,i);
     end
 end
